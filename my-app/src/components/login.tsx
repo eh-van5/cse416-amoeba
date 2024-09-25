dfedc93c4bef612f434f509122deed41302cd328
KurisuStina <lin123evan@gmail.com>
2024-09-25 00:16:30 -0400

    init login page

---
+++ my-app/src/components/login.tsx
@@ -0,0 +1,19 @@
+export default function Login(){
+    return (
+        <div>
+            <div>
+                <img src="my-app\src\images\colony-logo-transparent.png" alt="app logo" />
+                <h1>Colony</h1>
+            </div>
+            <div>
+                <h2>Login</h2>
+                <input type="text" />
+                <button type="button">Continue</button>
+
+                <p><i>Don't have an account?</i></p>
+                <p><i><u>Register</u></i></p>
+                <p><i><u>Forgot password?</u></i></p>
+            </div>
+        </div>
+    )
+}
