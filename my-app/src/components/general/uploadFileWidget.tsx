f6451105a5da952aa54cc221f09047dd4ea0b40e
Elizabeth Sham <elizabeth.sham@stonybrook.edu>
2024-10-07 22:21:25 -0400

    did a loading bar that only works for one item

--- my-app/src/components/general/uploadFileWidget.tsx
+++ my-app/src/components/general/uploadFileWidget.tsx
@@ -3,25 +3,41 @@ import { UploadFileIcon } from "../../images/icons/icons";
 export default function UploadFileWidget(){
     function readFile(file: File){
         const reader = new FileReader();
-        reader.addEventListener('load', (event) =>{
-            if (event.target == null) {
-                throw Error;
+        reader.addEventListener('loadstart', (function(f) {
+            return (event) =>{
+                const loadingBar = document.getElementById("file-progress-bar");
+                if (loadingBar){
+                    // loadingBar.innerHTML = "Loading " + file.name;
+                    loadingBar.style.width = "0%";
+                } 
             }
-            // const result = event.target.result;
-            // console.log(result);
-        })
-        reader.addEventListener('progress', (event) => {
-            if (event.loaded && event.total) {
-                const percent = (event.loaded / event.total) * 100;
-                const loadingBar = document.getElementById("file-drop-zone");
-                console.log(`Progress: ${Math.round(percent)}`)
+        })(file))
+        reader.addEventListener('progress', (function(f) {
+            return (event) => {
+                if (event.loaded && event.total) {
+                    const percent = (event.loaded / event.total) * 100;
+                    const loadingBar = document.getElementById("file-progress-bar");
+                    const files = document.getElementById("files");
+                    if (loadingBar && files){
+                        console.log(f.name);
+                        if (percent >= 100){
+                            loadingBar.style.width = "100%";
+                            files.innerHTML = "Loaded " + f.name;
+                        } else {
+                            loadingBar.style.width = percent + "%";
+                        }
+                    }
+
+                    console.log(`Progress: ${Math.round(percent)}`)
+                }
             }
-        });
+        })(file));
         reader.readAsDataURL(file);
     }
     function dropHandler(event: React.DragEvent){
         event.preventDefault();
         console.log("Something has been dropped");
+
         Array.from(event.dataTransfer.items).forEach((item, i) => {
             if (item.kind === "file"){
                 const file = item.getAsFile();
@@ -29,7 +45,6 @@ export default function UploadFileWidget(){
                     throw Error("Parsing something that isn't a file");
                 }
                 readFile(file);
-                console.log(`... file[${i}].name = ${file.name}`);
             }
         })
     }
@@ -42,22 +57,31 @@ export default function UploadFileWidget(){
         if (filesList === null){
             throw Error;
         }
+
         Object.values(filesList).forEach(file => {
             readFile(file);
-        })        
+        })
     }
     return (
-        <div             
-        onDrop={dropHandler} 
-        onDragOver={dragOverHandler}
-        id = "file-drop-zone" > 
-            <label id = "file-widget" htmlFor="file-upload">
-                <br /> {UploadFileIcon()}
-                <p>Drag and Drop</p>
-                <p>or</p>
-                <label htmlFor="file-upload" id="file-upload-label"><u>Browse</u></label>
-                <input type="file" id="file-upload" multiple onChange={fileSelectorHandler} />
-            </label>
+        <div id = "file-upload-widget">
+            <div             
+            onDrop={dropHandler} 
+            onDragOver={dragOverHandler}
+            id = "file-drop-zone" > 
+                <label id = "file-widget" htmlFor="file-upload">
+                    <br /> {UploadFileIcon()}
+                    <p>Drag and Drop</p>
+                    <p>or</p>
+                    <label htmlFor="file-upload" id="file-upload-label"><u>Browse</u></label>
+                    <input type="file" id="file-upload" multiple onChange={fileSelectorHandler} />
+                </label>
+            </div>
+            <div id = "file-progress">
+                <div id = "file-progress-bar">
+                </div>
+            </div>
+            <div id = "files">
+            </div>
         </div>
     )
 }
