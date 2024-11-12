cdf5dfd7f09c939fd17dd88fe35df3a70e266df7
KurisuStina <lin123evan@gmail.com>
2024-11-12 15:14:44 -0500

    connected btcctl to btcwallet

--- my-app/src/scripts/btcd_wrapper.sh
+++ my-app/src/scripts/btcd_wrapper.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Wrapper script for btcd with specific flags
 
-/mnt/c/Users/Evan/Documents/SBU/2024Fall/CSE416/cse416-amoeba/my-app/src/coin/btcd/./btcd --rpcuser=user --rpcpass=password --notls --debuglevel=info --miningaddr=1ANiT1wNVVPrnHyafGDpvtnYJHVUpZ6n1t "$@"
+echo "Starting btcd wrapper script" >&2
+/mnt/c/Users/Evan/Documents/SBU/2024Fall/CSE416/cse416-amoeba/my-app/src/coin/btcd/./btcd --rpcuser=user --rpcpass=password --notls --debuglevel=info "$@"
+echo "btcd finished running" >&2
