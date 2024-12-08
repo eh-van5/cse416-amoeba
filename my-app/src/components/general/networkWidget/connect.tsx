33925036658cb283e331de390ce11dbc743255b7
evan <lin123evan@gmail.com>
2024-12-07 20:32:49 -0500

    merge

--- my-app/src/components/general/networkWidget/connect.tsx
+++ my-app/src/components/general/networkWidget/connect.tsx
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import { useTheme } from "../../../ThemeContext";
-
-export default function Connect() {
-    const  {isDarkMode} = useTheme();
-    return (
-        <div id = "connection-container" style={(isDarkMode ? {backgroundColor:'#215F64'} : {})}>
-            <label htmlFor="networkUrl" style={(isDarkMode ? {color: 'white'} : {})}>Connect To Network</label>
-            <br></br>
-            <input placeholder="Link" id="networkUrl" type="text"/>
-            <input value="Connect" type="submit"/>
-        </div>
-    );
-=======
 import { useAppContext } from "../../../AppContext";
 
 export default function Connect() {
@@ -24,5 +10,4 @@ export default function Connect() {
             <input value="Connect" type="submit"/>
         </div>
     );
->>>>>>> main
 }
--- my-app/src/components/general/simpleBox.tsx
+++ my-app/src/components/general/simpleBox.tsx
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import { ThreeDotIcon } from "../../images/icons/icons";
-import { useTheme } from "../../ThemeContext";
-
-interface simpleBoxProps {
-    title: string;
-    style?: React.CSSProperties;
-    children?: React.ReactNode;
-    dotIcon?:boolean;
-}
-
-const SimpleBox: React.FC<simpleBoxProps> = ({title, style, children,dotIcon = false}) => {
-    const {isDarkMode} = useTheme();
-    return (
-        <div className={`box-container${isDarkMode ? '-dark' : ''}`} style={style}>
-            <div className="box-header">
-                <h3 className="box-title">{title}</h3>
-                {dotIcon === true && (
-                <div className="box-header-button">
-                    <ThreeDotIcon />
-                </div>
-                )}    
-            </div>
-            {children}
-        </div>
-    );
-}
-
-=======
 import { ThreeDotIcon } from "../../images/icons/icons";
 import { useAppContext } from "../../AppContext";
 
@@ -54,5 +25,4 @@ const SimpleBox: React.FC<simpleBoxProps> = ({title, style, children,dotIcon = f
     );
 }
 
->>>>>>> main
 export default SimpleBox;
